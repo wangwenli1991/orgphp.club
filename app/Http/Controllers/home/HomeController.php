b8cb4021e6e625436632bcf2f6a2cc18b27e0a51
wangwenli1991 <1527179450@qq.com>
2019-01-15 06:51:10 +0800

    'testcurd'

--- app/Http/Controllers/home/HomeController.php
+++ app/Http/Controllers/home/HomeController.php
@@ -6,6 +6,8 @@ use App\c;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use DB;
+
 class HomeController extends Controller
 {
     /**
@@ -16,7 +18,36 @@ class HomeController extends Controller
     public function index()
     {
         //
-    $products=  \DB::table('products')->get();
+    $products=  DB::table('products')->get();
+
+
+//    DB::table('users')
+//        ->insert([
+//            'name'=>'asdf','email'=>'asdf@sadf.com','password'=>'sadfkj',
+//            'name'=>'wangwenli','email'=>'asdf@qq.com','password'=>'wang@wenli.com'
+//        ]);
+
+//
+//    //删
+//        DB::table('users')
+//            ->where('id','>',4)
+//            ->delete();
+
+        //改
+
+//        DB::table('users')
+//            ->where('id','=',4)
+//            ->update(['name'=>'wang5','email'=>'wang@wang.com','password'=>bcrypt('12345')]);
+
+
+//        //查询
+//        $users=DB::table('users')
+//            ->where('id','>',2)
+//            ->get('email');
+
+
+//        $email = DB::table('users')->where('name', 'wang5')->value('email');
+
 
 
         return view('home.index',compact('products'));
