2109da73957409d128524559000e7a5657c679d3
wangwenli1991 <1527179450@qq.com>
2019-01-15 19:31:11 +0800

    'task'

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+use App\Task;
+use Illuminate\Http\Request;
 
 
 Route::get('/home', 'HomeController@index')->name('home');
@@ -41,3 +42,47 @@ Route::get('/admin', 'Admin\AdminController@index');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+
+
+
+//task
+
+
+Route::group(['middleware' => ['web']], function () {
+    /**
+     * Show Task Dashboard
+     */
+    Route::get('/task', function () {
+        return view('tasks', [
+            'tasks' => Task::orderBy('created_at', 'asc')->get()
+        ]);
+    });
+    /**
+     * Add New Task
+     */
+
+
+    Route::post('/task', function (Request $request) {
+        $validator = Validator::make($request->all(), [
+//            'name' => 'required|max:255',
+        ]);
+        if ($validator->fails()) {
+            return redirect('/task')
+                ->withInput()
+                ->withErrors($validator);
+        }
+        $task = new Task;
+        $task->name = $request->name;
+        $task->save();
+        return redirect('/task');
+    });
+    /**
+     * Delete Task
+     */
+    Route::delete('/task/{id}', function ($id) {
+        Task::findOrFail($id)->delete();
+        return redirect('/task');
+    });
+});
